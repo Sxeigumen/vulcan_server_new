30bc6cdfa0a5e45b695f558dc843a48b2fba32a8
Sxeigumen <krasnov619@yandex.ru>
2023-07-11 19:56:54 +0300

    Second

--- ServerIoT/server.py
+++ ServerIoT/server.py
@@ -1,2 +1,32 @@
-import amqtt 
+from paho.mqtt import client as mqtt_client
 
+#Brocker info
+broker = '10.0.41.165'	# broker ip
+port = 9001 # 9001 or 1883
+
+#Client-Server info
+my_topics = [
+	('game/data', 1),
+	('instructions', 1)
+]
+client_id = f'publish-{999}' # generate this
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe(my_topics)
+    print(0)
+
+
+def client_forever():
+
+	client = mqtt_client.Client()
+	client.on_connect = on_connect
+
+	client.connect(broker, port, 60)
+	client.loop_forever()
+
+
+
+if __name__ == "__main__":  
+    client_forever()
